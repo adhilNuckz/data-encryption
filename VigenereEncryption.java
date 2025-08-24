7e4c24f264b16440d5fd11ef4c266a6c9d12bead
aadhilNuckz <mikeadhil2002@gmail.com>
2025-08-25 02:35:15 +0530

    added vigenereEncryption

---
+++ VigenereEncryption.java
@@ -0,0 +1,80 @@
+class VigenereEncryption {
+
+    char[][] vigenere = new char[26][26];
+
+    VigenereEncryption() {
+
+        for (int i = 0; i < 26; i++) {
+
+            for (int j = 0; j < 26; j++) {
+
+                vigenere[i][j] = (char) ((i + j) % 26 + 'A');
+            }
+
+        }
+
+    }
+
+
+
+    //  Encryption 
+
+    public String encrypt(String Message, String Key) {
+
+        Message = Message.replaceAll("\\s", "").toUpperCase();
+        Key = Key.toUpperCase();
+
+        StringBuilder encryptedMessage = new StringBuilder();
+
+        for (int i = 0; i < Message.length(); i++) {
+            char originalChar = Message.charAt(i);
+            char keyChar = Key.charAt(i % Key.length());
+
+            encryptedMessage.append(vigenere[keyChar - 'A'][originalChar - 'A']);
+
+        }
+
+        return encryptedMessage.toString();
+
+    }
+
+
+
+
+
+
+    //  Decryption   
+
+
+
+    public void   decrypt (String EncryptedMessage ,  String Key  ) {
+
+            
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        String Message = "I WILL KILL YOU";
+        String Key = "FUCK";
+
+        VigenereEncryption secret = new VigenereEncryption();
+
+        String SecretMessage = secret.encrypt(Message, Key);
+
+        System.out.println("Encrypted Message : " + SecretMessage);
+
+    }
+
+}
